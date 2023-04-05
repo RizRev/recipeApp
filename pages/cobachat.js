ab0a0ec6de10c629eaa190acaafd6ebe855e5b72
RizRev <114935602+RizRev@users.noreply.github.com>
2023-04-05 16:03:40 +0700

    masukin chat

---
+++ pages/cobachat.js
@@ -0,0 +1,9 @@
+function Chat () {
+    return (
+        <div>
+        <div class="pubble-app" data-app-id="118944" data-app-identifier="118944"></div>
+        <script type="text/javascript" src="https://cdn.chatify.com/javascript/loader.js" defer></script>
+        </div>
+    )
+}
+export default Chat
